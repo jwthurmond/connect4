cb867e10efca039cf33301af30f27105e478936a
Wes Thurmond <jwthurmond@gmail.com>
2023-10-27 19:51:38 -0700

    Add project files.

---
+++ connect4.library/Board.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel;
+
+namespace connect4.library
+{
+    public class Board
+    {
+        public Board()
+        {
+            //initialize the board
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+
+                    board[i, j] = 0;
+                }
+            }
+            Player = 1;
+        }
+        public int[,] board = new int[6, 7];
+        public int[] column = new int[7];
+        public int[] row = new int[6];
+        public int[] diag = new int[12];
+        public int[] rdiag = new int[12];
+        public int[] moves = new int[42];
+        public int moveCount = 0;
+        public int Player { get; set; }
+        public int winner = 0;
+        public int move = 0;
+        public int moveRow = 0;
+        public int moveCol = 0;
+        public int moveDiag = 0;
+        public int moveRDiag = 0;
+        public int moveCountCol = 0;
+        public int moveCountRow = 0;
+        public int moveCountDiag = 0;
+        public int moveCountRDiag = 0;
+        public int moveCountCol1 = 0;
+        public int moveCountRow1 = 0;
+        public int moveCountDiag1 = 0;
+        public int moveCountRDiag1 = 0;
+        public int moveCountCol2 = 0;
+        public int moveCountRow2 = 0;
+        public int moveCountDiag2 = 0;
+        public int moveCountRDiag2 = 0;
+        public int moveCountCol3 = 0;
+        public int moveCountRow3 = 0;
+        public int moveCountDiag3 = 0;
+        public int moveCountRDiag3 = 0;
+        public int moveCountCol4 = 0;
+        public int moveCountRow4 = 0;
+        public int moveCountDiag4 = 0;
+        public int moveCountRDiag4 = 0;
+        public int moveCountCol5 = 0;
+        public int moveCountRow5 = 0;
+        public int moveCountDiag5 = 0;
+        public int moveCountRDiag5 = 0;
+        public int moveCountCol6 = 0;
+        public int moveCountRow6 = 0;
+        public int moveCountDiag6 = 0;
+        public int moveCountRDiag6 = 0;
+        public int moveCountCol7 = 0;
+        public int moveCountRow7 = 0;
+        public int moveCountDiag7 = 0;
+
+        public int GetPlayer()
+        {
+            var currentMove = moveCount + 1;
+            if (currentMove % 2 == 0)
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public Board Move(Board board, int column)
+        {
+            var columnMove = column - 1;
+            var row = GetMoveRow(board, columnMove);
+            var validation = IsValidMove(board, row, columnMove);
+            if (validation == null)
+            {
+                board = MakeMove(board, row, columnMove);
+                CheckWinner(board);
+            }
+            else
+            {
+
+                throw new InvalidOperationException($"Invalid Move: {validation}");
+            }
+            return board;
+
+        }
+        private Board MakeMove(Board board, int row, int column)
+        {
+            board.moveCount++;
+            if (board.moveCount % 2 == 0)
+            {
+                board.Player = 2;
+            }
+            else
+            {
+                board.Player = 1;
+            }
+            board.board[row, column] = board.Player;
+            return board;
+        }
+        private int GetMoveRow(Board board, int columnMove)
+        {
+            //get the row of the move
+            for (int i = 5; i >= 0; i--)
+            {
+                if (board.board[i, columnMove] == 0)
+                {
+                    board.moveRow = i;
+                    break;
+                }
+            }
+            return board.moveRow;
+        }
+
+        private string? IsValidMove(Board board, int row, int column)
+        {
+            if (row < 0 || row > 6)
+            {
+                return "Row is not valid";
+            }
+            if (column < 0 || column > 7)
+            {
+                return "Column is not valid";
+            }
+
+            if (board.board[row, column] == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return "Move already taken";
+            }
+        }
+
+        private void CheckWinner(Board board)
+        {
+            //check for a winner
+            if (board.moveCountCol == 4 || board.moveCountRow == 4 || board.moveCountDiag == 4 || board.moveCountRDiag == 4)
+            {
+                board.winner = board.Player;
+            }
+        }
+
+    }
+}
---
+++ connect4/Program.cs
@@ -0,0 +1,43 @@
+﻿using connect4.library;
+
+namespace connect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new Board();
+            PrintBoard(board);
+            while (board.winner == 0)
+            {
+                try
+                {
+                    Console.Write($"Player {board.GetPlayer()} Enter the Column:");
+                    var column = int.Parse(Console.ReadLine());
+                    board = board.Move(board, column);
+                    PrintBoard(board);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.ToString());
+                }
+            }
+        }
+
+        private static void PrintBoard(Board board)
+        {
+            //write the current state of the board to the console
+            Console.WriteLine(" 1 2 3 4 5 6 7");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(board.board[i, j]);
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
