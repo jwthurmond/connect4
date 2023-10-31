9ac135dfcbd2d4200a6c5023dae843742f96f80a
Wes Thurmond <jwthurmond@users.noreply.github.com>
2023-10-31 13:53:59 -0700

    Wes/tournament runner (#8)
    * updating to run a tournament
    
    * adding parameters

---
+++ connect4.tournament/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace connect4.tournament
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tournament = new tournamentrunner.Tournament(3);
+            tournament.AddPlayer(new tournamentrunner.RandomPlayer());
+            tournament.AddPlayer(new tournamentrunner.IncrementBy1());
+            tournament.AddPlayer(new tournamentrunner.Always4());
+            tournament.Run(true);
+            
+            
+        }
+    }
+}
